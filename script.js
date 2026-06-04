d0c98d37deee1d9cc578624068f2ddbc337360bc
Sakthi002-D <sakthivel.d002@gmail.com>
2026-06-04 12:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
+const boldWords = document.querySelector("strong");
 
+
+	boldWords.forEach((word)  => {
+		word.style.color = "rgb(0, 128, 0)";
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
+	const boldWords = document.querySelectorAll("strong");
 
+	boldWords.forEach((word)  => {
+		word.style.color = "egb(0, 0, 0)";
+	})
     
 }
